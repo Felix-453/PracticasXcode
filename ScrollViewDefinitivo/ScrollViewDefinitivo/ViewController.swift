453cdc4d881daf99a8d7eab9e452e6c2fdead88a
Felix <al19020270@itsa.edu.mx>
2022-11-14 15:05:41 -0600

    Actualizacion 14-nov

---
+++ ScrollViewDefinitivo/ScrollViewDefinitivo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ScrollViewDefinitivo
+//
+//  Created by IMAC17 on 14/11/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
