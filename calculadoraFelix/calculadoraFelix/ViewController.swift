5f7b3004b72428c66e68e0cb6a1bb1ce3f9720d7
Felix <al19020270@itsa.edu.mx>
2022-11-15 15:23:56 -0600

    Actualizacion15-nov

---
+++ calculadoraFelix/calculadoraFelix/ViewController.swift
@@ -0,0 +1,86 @@
+//
+//  ViewController.swift
+//  calculadoraFelix
+//
+//  Created by IMAC17 on 15/11/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    var resultado: String = ""
+    
+    @IBOutlet weak var LabelResultado: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        
+    }
+    
+    @IBAction func btn0(_ sender: Any) {
+        
+    }
+    
+    
+    @IBAction func btn1(_ sender: Any) {
+    }
+    
+    @IBAction func btn2(_ sender: Any) {
+    }
+    
+    
+    @IBAction func btn3(_ sender: Any) {
+    }
+    @IBAction func btn4(_ sender: Any) {
+    }
+    
+    @IBAction func btn5(_ sender: Any) {
+    }
+    
+    @IBAction func btn6(_ sender: Any) {
+    }
+    
+    @IBAction func btn7(_ sender: Any) {
+    }
+    
+    
+    @IBAction func btn8(_ sender: Any) {
+    }
+    
+    @IBAction func btn9(_ sender: Any) {
+    }
+    
+    
+    @IBAction func btnPunto(_ sender: Any) {
+    }
+    
+    @IBAction func btnIgual(_ sender: Any) {
+    }
+    
+    @IBAction func btnSuma(_ sender: Any) {
+    }
+    
+    @IBAction func btnResta(_ sender: Any) {
+    }
+    
+    @IBAction func btnMulti(_ sender: Any) {
+    }
+    
+    
+    @IBAction func btnDiv(_ sender: Any) {
+    }
+    
+    @IBAction func btnModulo(_ sender: Any) {
+    }
+    
+    @IBAction func btnMasMenos(_ sender: Any) {
+    }
+    
+    @IBAction func btnClear(_ sender: Any) {
+    }
+    
+}
+
